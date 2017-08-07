f980febefe94c5fdee6e05ded86d9c3581bc3332
zgj12138 <784883728@qq.com>
2017-08-07 23:57:59 +0800

    删除修改日志的dao和job entity

---
+++ src/main/java/io/kevin/modules/job/entity/ScheduleJobEntity.java
@@ -0,0 +1,105 @@
+package io.kevin.modules.job.entity;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 定时器
+ * @author ZGJ
+ * @date 2017/8/7 23:51
+ **/
+public class ScheduleJobEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 任务调度参数key
+     */
+    public static final String JOB_PARAM_KEY = "JOB_PARAM_KEY";
+    /**
+     * 任务id
+     */
+    private Long JobId;
+    /**
+     * spring bean名称
+     */
+    @NotBlank(message = "bean名称不能为空")
+    private String beanName;
+    /**
+     * 参数
+     */
+    private String params;
+    /**
+     * cron表达式
+     */
+    @NotBlank(message = "cron表达式不能为空")
+    private String cronExpression;
+    /**
+     * 任务状态 0：正常 1：暂停
+     */
+    private Integer status;
+    /**
+     * 备注
+     */
+    private String remark;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    public Long getJobId() {
+        return JobId;
+    }
+
+    public void setJobId(Long jobId) {
+        JobId = jobId;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
