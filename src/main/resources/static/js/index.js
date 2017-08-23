b497a72800be15c4e8cbac2a15ecc7a408162518
zgj12138 <784883728@qq.com>
2017-08-23 23:21:50 +0800

    修改部分前端页面

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,8 @@
 //生成菜单
 var menuItem = Vue.extend({
     name: 'menu-item',
-    props:{item:{},index:0},
-    template:[
+    props: { item: {}, index: 0 },
+    template: [
         '<li :class="{active: (item.type===0 && index === 0)}">',
         '<a v-if="item.type === 0" href="javascript:;">',
         '<i v-if="item.icon != null" :class="item.icon"></i>',
@@ -22,74 +22,74 @@ var menuItem = Vue.extend({
 
 //iframe自适应
 $(window).on('resize', function() {
-	var $content = $('.content');
-	$content.height($(this).height() - 120);
-	$content.find('iframe').each(function() {
-		$(this).height($content.height());
-	});
+    var $content = $('.content');
+    $content.height($(this).height() - 120);
+    $content.find('iframe').each(function() {
+        $(this).height($content.height());
+    });
 }).resize();
 
 //注册菜单组件
-Vue.component('menuItem',menuItem);
+Vue.component('menuItem', menuItem);
 
 var vm = new Vue({
-	el:'#rrapp',
-	data:{
-		user:{},
-		menuList:{},
-		main:"main.html",
-		password:'',
-		newPassword:'',
-        navTitle:"欢迎页"
-	},
-	methods: {
-		getMenuList: function () {
-			$.getJSON(baseURL + "sys/menu/nav", function(r){
-				vm.menuList = r.menuList;
+    el: '#gjapp',
+    data: {
+        user: {},
+        menuList: {},
+        main: "main.html",
+        password: '',
+        newPassword: '',
+        navTitle: "欢迎页"
+    },
+    methods: {
+        getMenuList: function() {
+            $.getJSON(baseURL + "sys/menu/nav", function(r) {
+                vm.menuList = r.menuList;
                 window.permissions = r.permissions;
-			});
-		},
-		getUser: function(){
-			$.getJSON(baseURL + "sys/user/info", function(r){
-				vm.user = r.user;
-			});
-		},
-		updatePassword: function(){
-			layer.open({
-				type: 1,
-				skin: 'layui-layer-molv',
-				title: "修改密码",
-				area: ['550px', '270px'],
-				shadeClose: false,
-				content: jQuery("#passwordLayer"),
-				btn: ['修改','取消'],
-				btn1: function (index) {
-					var data = "password="+vm.password+"&newPassword="+vm.newPassword;
-					$.ajax({
-						type: "POST",
-					    url: baseURL + "sys/user/password",
-					    data: data,
-					    dataType: "json",
-					    success: function(r){
-							if(r.code == 0){
-								layer.close(index);
-								layer.alert('修改成功', function(){
-									location.reload();
-								});
-							}else{
-								layer.alert(r.msg);
-							}
-						}
-					});
-	            }
-			});
-		},
-        logout: function () {
+            });
+        },
+        getUser: function() {
+            $.getJSON(baseURL + "sys/user/info", function(r) {
+                vm.user = r.user;
+            });
+        },
+        updatePassword: function() {
+            layer.open({
+                type: 1,
+                skin: 'layui-layer-molv',
+                title: "修改密码",
+                area: ['550px', '270px'],
+                shadeClose: false,
+                content: jQuery("#passwordLayer"),
+                btn: ['修改', '取消'],
+                btn1: function(index) {
+                    var data = "password=" + vm.password + "&newPassword=" + vm.newPassword;
+                    $.ajax({
+                        type: "POST",
+                        url: baseURL + "sys/user/password",
+                        data: data,
+                        dataType: "json",
+                        success: function(r) {
+                            if (r.code == 0) {
+                                layer.close(index);
+                                layer.alert('修改成功', function() {
+                                    location.reload();
+                                });
+                            } else {
+                                layer.alert(r.msg);
+                            }
+                        }
+                    });
+                }
+            });
+        },
+        logout: function() {
             $.ajax({
                 type: "POST",
                 url: baseURL + "sys/logout",
                 dataType: "json",
-                success: function(r){
+                success: function(r) {
                     //删除本地token
                     localStorage.removeItem("token");
                     //跳转到登录页面
@@ -97,7 +97,7 @@ var vm = new Vue({
                 }
             });
         },
-        donate: function () {
+        donate: function() {
             layer.open({
                 type: 2,
                 title: false,
@@ -107,40 +107,40 @@ var vm = new Vue({
                 content: ['http://cdn.renren.io/donate.jpg', 'no']
             });
         }
-	},
-	created: function(){
-		this.getMenuList();
-		this.getUser();
-	},
-	updated: function(){
-		//路由
-		var router = new Router();
-		routerList(router, vm.menuList);
-		router.start();
-	}
+    },
+    created: function() {
+        this.getMenuList();
+        this.getUser();
+    },
+    updated: function() {
+        //路由
+        var router = new Router();
+        routerList(router, vm.menuList);
+        router.start();
+    }
 });
 
 
 
-function routerList(router, menuList){
-	for(var key in menuList){
-		var menu = menuList[key];
-		if(menu.type == 0){
-			routerList(router, menu.list);
-		}else if(menu.type == 1){
-			router.add('#'+menu.url, function() {
-				var url = window.location.hash;
-				
-				//替换iframe的url
-			    vm.main = url.replace('#', '');
-			    
-			    //导航菜单展开
-			    $(".treeview-menu li").removeClass("active");
+function routerList(router, menuList) {
+    for (var key in menuList) {
+        var menu = menuList[key];
+        if (menu.type == 0) {
+            routerList(router, menu.list);
+        } else if (menu.type == 1) {
+            router.add('#' + menu.url, function() {
+                var url = window.location.hash;
+
+                //替换iframe的url
+                vm.main = url.replace('#', '');
+
+                //导航菜单展开
+                $(".treeview-menu li").removeClass("active");
                 $(".sidebar-menu li").removeClass("active");
-			    $("a[href='"+url+"']").parents("li").addClass("active");
-			    
-			    vm.navTitle = $("a[href='"+url+"']").text();
-			});
-		}
-	}
+                $("a[href='" + url + "']").parents("li").addClass("active");
+
+                vm.navTitle = $("a[href='" + url + "']").text();
+            });
+        }
+    }
 }
